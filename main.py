ce486bd1bcb156438eb3574505dedb3ab9794228
old-willow <robert.kolozsi@gmail.com>
2015-11-19 12:45:47 +0100

    Basic functionality of FileChooser widget.

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ from kivy.app import App
 #from kivy.uix.widget import Widget
 #from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
 from kivy.properties import ObjectProperty
+from kivy.factory import Factory
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
 #from kivy.uix.button import Button
+
+import os
 
 
 #class MainButtons(Button):
 #    padding = ReferenceListProperty(None)
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 
 class Box(BoxLayout):
@@ -20,17 +29,37 @@ class Box(BoxLayout):
 
     #layout.add_widget(btn_cancel)
     #layout.add_widget(btn_send)
-    button_cancel = ObjectProperty(None)
-    button_send = ObjectProperty(None)
 
-    def select(self, *args):
-        """
-        This will be called from kv file when user selects a file.
-        """
-        try:
-            self.file_path_label.text = args[1][0]
-        except:
-            pass
+    loadfile = ObjectProperty(None)
+    file_path_label = ObjectProperty(None)
+    #button_cancel = ObjectProperty(None)
+    #button_send = ObjectProperty(None)
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        print("hello I'm firing!")
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load attachement", content=content,
+                            size_hint=(0.9, 0.9))
+
+        self._popup.open()
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            self.file_path_label.text = stream.read()
+
+        self.dismiss_popup()
+
+    #def select(self, *args):
+    #    """
+    #    This will be called from kv file when user selects a file.
+    #    """
+    #    try:
+    #        self.file_path_label.text = args[1][0]
+    #    except:
+    #        pass
 
     def exit(self):
         App.get_running_app().stop()
@@ -67,6 +96,8 @@ class SendEmailsApp(App):
 #        manager = Manager(transition=WipeTransition())
 #        return manager
 
+Factory.register('Box', cls=Box)
+Factory.register('LoadDialog', cls=LoadDialog)
 
 if __name__ == '__main__':
     SendEmailsApp().run()
